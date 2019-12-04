6c1076362c21eb75a6297550cd00d350cdc8b676
Sagar Jaybhay <sagarjaybhay18091988@gmail.com>
2019-12-04 10:50:41 +0530

    added constant file

---
+++ constants.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+const b string = "sagar jaybhay"
+const a = 42
+
+const (
+	v = iota
+	j = iota * 10
+	k = iota * 10
+)
+
+func main() {
+
+	const aa = "sagar"
+	fmt.Println("hello brother")
+	println(v)
+	println(j)
+	println(k)
+}
