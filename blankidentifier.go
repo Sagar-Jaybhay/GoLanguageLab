2f90c681cb8808be26bee3268ca1dd572ddc3b1f
Sagar Jaybhay <sagarjaybhay18091988@gmail.com>
2019-12-04 10:10:05 +0530

    added blankidentifier

---
+++ blankidentifier.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+
+	res, _ := http.Get("https://www.golang.org")
+	page, _ := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	fmt.Printf("%s", page)
+
+}
