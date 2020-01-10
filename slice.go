e956f51bacc56309d854d9b6d53da265fb1a6843
Sagar Jaybhay <sagarjaybhay18091988@gmail.com>
2020-01-10 10:07:55 +0530

    added slice file

--- slice.go
+++ slice.go
@@ -3,12 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	primes := [6]int{2, 3, 5, 7, 11, 13}
 
-	//fmt.Printf("%v %T", primes)
-	fmt.Println("Printing complete slice", primes)
-	var s []int = primes[1:4] // this is slicing the slice
-	fmt.Println(primes[2])    //index access ; access by index
+	/*
+		primes := [6]int{2, 3, 5, 7, 11, 13}
+		fmt.Printf("%v %T", primes)
+		fmt.Println("Printing complete slice", primes)
+		var s []int = primes[1:4] // this is slicing the slice
+		fmt.Println(primes[2])    //index access ; access by index
+		fmt.Println(s)
+	*/
 
-	fmt.Println(s)
+	var myslice []string
+	fmt.Print(myslice)
+    fmt.Printf()
 }
