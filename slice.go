122a1ef1bd4360203b52a2bd0efc2d01a17c8d74
Sagar Jaybhay <sagarjaybhay18091988@gmail.com>
2020-01-10 16:42:40 +0530

    remove comments

--- slice.go
+++ slice.go
@@ -4,50 +4,48 @@ import "fmt"
 
 func main() {
 
-	/*
-				primes := [6]int{2, 3, 5, 7, 11, 13}
-				fmt.Printf("%v %T", primes)
-				fmt.Println("Printing complete slice", primes)
-				var s []int = primes[1:4] // this is slicing the slice
-				fmt.Println(primes[2])    //index access ; access by index
-				fmt.Println(s)
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Printf("%v %T", primes)
+	fmt.Println("Printing complete slice", primes)
+	var s []int = primes[1:4] // this is slicing the slice
+	fmt.Println(primes[2])    //index access ; access by index
+	fmt.Println(s)
 
+	var myslice []string
+	fmt.Print(myslice)
+	fmt.Printf()
 
-			var myslice []string
-			fmt.Print(myslice)
-			fmt.Printf()
+	//------------------ appending elements to slice beyond it capacity -----------------------------
+	myslice := make([]string, 3, 5)
+	// 3 is length:- it is number of elements refered by slice
+	// 5 is capacity:- it is number of elements in underlying array.
 
-		//------------------ appending elements to slice beyond it capacity -----------------------------
-		myslice := make([]string, 3, 5)
-		// 3 is length:- it is number of elements refered by slice
-		// 5 is capacity:- it is number of elements in underlying array.
+	myslice[0] = "Sagar"
+	myslice[1] = "Jaybhay"
+	myslice[2] = "Sr. Software Developer"
 
-		myslice[0] = "Sagar"
-		myslice[1] = "Jaybhay"
-		myslice[2] = "Sr. Software Developer"
+	fmt.Println(myslice)
+	fmt.Println("Orignal Length ", len(myslice))
+	fmt.Println("Orignal Capcity ", cap(myslice))
 
-		fmt.Println(myslice)
-		fmt.Println("Orignal Length ", len(myslice))
-		fmt.Println("Orignal Capcity ", cap(myslice))
+	myslice = append(myslice, "Pune")
+	myslice = append(myslice, "Maharashtra")
+	myslice = append(myslice, "India")
 
-		myslice = append(myslice, "Pune")
-		myslice = append(myslice, "Maharashtra")
-		myslice = append(myslice, "India")
+	fmt.Println("Updated ", myslice)
+	fmt.Println("Updated Length ", len(myslice))
+	fmt.Println("Updated Capcity ", cap(myslice))
 
-		fmt.Println("Updated ", myslice)
-		fmt.Println("Updated Length ", len(myslice))
-		fmt.Println("Updated Capcity ", cap(myslice))
-	*/
 	// ----------- calling different ways to create the slice ---------------------
 
-	//differentWaystocreateSlice()
+	differentWaystocreateSlice()
 
 	//-----------call different ways to create slice
-	//shorthandsyntax()
+	shorthandsyntax()
 
 	//
 
-	//declarewithvar()
+	declarewithvar()
 
 	withmake()
 }
